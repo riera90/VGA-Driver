f20b5776c766b6d4cd40da84cd11a666eeb53ad1
Diego Rodríguez Riera <riera90@protonmail.com>
2020-11-01 20:21:29 +0100

    fixed headers

--- vgaPainter.vhd
+++ vgaPainter.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:37:30 10/25/2020 
--- Design Name: 
--- Module Name:    vgaPainter - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
